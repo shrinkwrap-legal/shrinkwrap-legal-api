74276e4fa054db1b9b5a285abf3cf690faa3fa6d
Bernhard Jungwirth <bernhard@it.jungwirth.me>
2024-01-23 20:40:37 +0100

    OpenAPI documentation

--- src/main/java/legal/shrinkwrap/api/controller/ShrinkwrapController.java
+++ src/main/java/legal/shrinkwrap/api/controller/ShrinkwrapController.java
@@ -1,15 +1,26 @@
 package legal.shrinkwrap.api.controller;
 
 
+import legal.shrinkwrap.api.dto.GeneralDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ShrinkwrapController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ShrinkwrapController.class);
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello, I am a shrink wrapper";
+    }
+
+
+    @GetMapping("/general")
+    public GeneralDto getGeneralInfo() {
+        LOG.info("general info called");
+        return new GeneralDto("Shringwrap.Legal", "0.0.1");
     }
 }
---
+++ src/main/java/legal/shrinkwrap/api/dto/GeneralDto.java
@@ -0,0 +1,4 @@
+package legal.shrinkwrap.api.dto;
+
+public record GeneralDto(String name, String version) {
+}
