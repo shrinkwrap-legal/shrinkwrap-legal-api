e12b40e5cd8c73b2762a7e082936b7dabaaf2679
Bernhard Jungwirth <bernhard@it.jungwirth.me>
2024-06-14 21:37:14 +0200

    spring ai vectorStore

---
+++ src/main/java/legal/shrinkwrap/api/config/SpringAIConfiguration.java
@@ -0,0 +1,41 @@
+package legal.shrinkwrap.api.config;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.Resource;
+
+import java.util.List;
+
+
+@Configuration
+public class SpringAIConfiguration {
+
+
+    @Profile("openai")
+    @Bean
+    VectorStore vectorStore(EmbeddingModel embedding, @Value("classpath:ASVG-01.06.2024.pdf") Resource pdf) {
+        SimpleVectorStore vectorStore = new SimpleVectorStore(embedding);
+        PagePdfDocumentReader reader = new PagePdfDocumentReader(pdf);
+        TokenTextSplitter splitter = new TokenTextSplitter();
+        List<Document> documents = splitter.apply(reader.get());
+        vectorStore.accept(documents);
+        return vectorStore;
+    }
+
+    @Profile("!openai")
+    @Bean("vectorStore")
+    VectorStore vectorStoreDummy(EmbeddingModel embedding) {
+        SimpleVectorStore vectorStore = new SimpleVectorStore(embedding);
+        return vectorStore;
+    }
+
+
+}
--- src/main/java/legal/shrinkwrap/api/controller/SpringChatClientController.java
+++ src/main/java/legal/shrinkwrap/api/controller/SpringChatClientController.java
@@ -1,22 +1,68 @@
 package legal.shrinkwrap.api.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * https://entwickler.de/reader/reading-collection/6662dda8be48cf002cbd6200/a1d1eb6d925500dcee7482fb
+ *
+ * https://spring.io/blog/2024/03/06/function-calling-in-java-and-spring-ai-using-the-latest-mistral-ai-api
+ */
 @RestController
 public class SpringChatClientController {
 
     private final ChatClient chatClient;
 
-    public SpringChatClientController(ChatClient.Builder chatClientBuilder) {
+    private final VectorStore vectorStore;
+
+    public SpringChatClientController(ChatClient.Builder chatClientBuilder, VectorStore vectorStore) {
         this.chatClient = chatClientBuilder.build();
+        this.vectorStore = vectorStore;
     }
 
     @GetMapping("/client")
     public String generation(String question) {
         String answer = chatClient.prompt().user(question).call().content();
         return answer;
+    }
+
+    @GetMapping("/pdf")
+    public String infoPdf(String question) {
+        List<Document> documents = vectorStore.similaritySearch(question);
+
+        String inlined = documents.stream()
+                .map(Document::getContent)
+                .collect(Collectors.joining(System.lineSeparator()));
+
+        String prompt = """
+                Du bist ein Rechtsanwalt im Bereich des Sozialversicherungsrechts.
+                Zur Beantwortung kannst Du in den Daten in DOCUMENTS nachsehen.
+                Es ist nur erlaubt antworten von DOCUMENTS zu verwenden.
+                Wenn du nicht sicher bist, dann schreib es als Antwort.
+                
+                DOCUMENTS:
+                {documents}
+                """;
+
+        Message system = new SystemPromptTemplate(prompt)
+                .createMessage(Map.of("documents", inlined));
+        UserMessage user = new UserMessage(question);
+        Prompt combinedPrompt = new Prompt(List.of(system, user));
+
+        String answer = chatClient.prompt(combinedPrompt).call().content();
+        return answer;
+
     }
 
 }
