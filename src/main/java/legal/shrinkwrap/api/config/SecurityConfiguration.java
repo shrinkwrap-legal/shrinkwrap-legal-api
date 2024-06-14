5b8778a889f5ceb9a0516e431384664c5d5dea95
Bernhard Jungwirth <bernhard@it.jungwirth.me>
2024-06-14 21:09:10 +0200

    spring security config

--- src/main/java/legal/shrinkwrap/api/config/SecurityConfiguration.java
+++ src/main/java/legal/shrinkwrap/api/config/SecurityConfiguration.java
@@ -1,7 +1,21 @@
 package legal.shrinkwrap.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors(httpSecurityCorsConfigurer ->
+                httpSecurityCorsConfigurer.configurationSource(request ->
+                        new CorsConfiguration().applyPermitDefaultValues()
+                )
+        );
+        return http.build();
+    }
 }
