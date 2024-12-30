e3bccdb33c9647e937ae3841ed972648ece8fa0c
Thomas Schreiber <thomas.schreiber@gmx.at>
2024-12-30 15:47:09 +0100

    for some reason, corenlp bean won't work

--- src/main/java/legal/shrinkwrap/api/config/NlpServiceConfiguration.java
+++ src/main/java/legal/shrinkwrap/api/config/NlpServiceConfiguration.java
@@ -11,7 +11,7 @@ public class NlpServiceConfiguration {
 
     @Bean("coreNlpService")
     public NLPService coreNlpService() {
-        return new CoreNlpServiceImpl();
+        return null;// new CoreNlpServiceImpl();
     }
 
     @Bean("openNlpService")
