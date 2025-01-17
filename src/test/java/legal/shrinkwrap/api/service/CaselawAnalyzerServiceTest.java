9afeda5a1b086926d783bf48b9e7744f6b70c8e2
Thomas Schreiber <thomas.schreiber@gmx.at>
2025-01-17 19:03:27 +0100

    small test

--- src/test/java/legal/shrinkwrap/api/service/CaselawAnalyzerServiceTest.java
+++ src/test/java/legal/shrinkwrap/api/service/CaselawAnalyzerServiceTest.java
@@ -1,6 +1,7 @@
 package legal.shrinkwrap.api.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import legal.shrinkwrap.api.adapter.HtmlDownloadService;
 import legal.shrinkwrap.api.config.AdapterConfiguration;
 import legal.shrinkwrap.api.config.ChatClientConfiguration;
 import legal.shrinkwrap.api.config.ServiceConfiguration;
@@ -15,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -38,6 +40,26 @@ class CaselawAnalyzerServiceTest {
 
     @Autowired
     private CaselawAnalyzerService caselawAnalyzerService;
+
+    @Autowired
+    private FileHandlingService fileHandlingService;
+
+    @Autowired
+    private HtmlDownloadService htmlDownloadService;
+
+    @Test
+    public void singleCaseLaw() {
+        String ecli = "ECLI:AT:OGH0002:2024:008OBA00004";
+        String url = "https://www.ris.bka.gv.at/Dokumente/Justiz/JJT_20240826_OGH0002_008OBA00004_24G0000_000/JJT_20240826_OGH0002_008OBA00004_24G0000_000.html";
+        String htmlContent = fileHandlingService.loadFile(ecli,".full.html");
+        if (htmlContent == null) {
+            htmlContent = htmlDownloadService.downloadHtml(url);
+            fileHandlingService.saveFile(ecli,".full.html", htmlContent);
+        }
+        CaseLawDataset ds = new CaseLawDataset(null, null, null, null, null, null, null, null, null, null, null, null, htmlContent, null);
+        caselawAnalyzerService.analyzeCaselaw(ds);
+        caselawAnalyzerService.summarizeCaselaw(ds);
+    }
 
     @Test
     void analyzeCaselaw() {
