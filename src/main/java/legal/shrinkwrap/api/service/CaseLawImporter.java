e4e41389b6f14e674b012743d45715993b0940a0
Thomas Schreiber <thomas.schreiber@gmx.at>
2025-03-24 23:13:19 +0100

    add logging

--- src/main/java/legal/shrinkwrap/api/service/CaseLawImporter.java
+++ src/main/java/legal/shrinkwrap/api/service/CaseLawImporter.java
@@ -7,6 +7,7 @@ import legal.shrinkwrap.api.adapter.ris.dto.RisCourt;
 import legal.shrinkwrap.api.adapter.ris.dto.RisJudikaturResult;
 import legal.shrinkwrap.api.adapter.ris.dto.RisSearchResult;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,7 @@ import java.time.Year;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class CaseLawImporter {
     private final DocumentService documentService;
     private final RisSoapAdapter risSoapAdapter;
@@ -40,6 +42,7 @@ public class CaseLawImporter {
             for (RisJudikaturResult result : results.getJudikaturResults()) {
                 documentService.importJudikaturResult(result);
             }
+            log.info("import done for " + court.name() + " with " + results.getJudikaturResults().size() + " documents");
         }
     }
 
