c8715d25b2ce2bcbf5587e548049b7eeb89e22b0
Thomas Schreiber <thomas.schreiber@gmx.at>
2025-03-25 17:50:54 +0100

    enable scheduling

--- src/main/java/legal/shrinkwrap/api/Application.java
+++ src/main/java/legal/shrinkwrap/api/Application.java
@@ -2,12 +2,11 @@ package legal.shrinkwrap.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {LiquibaseAutoConfiguration.class})
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
