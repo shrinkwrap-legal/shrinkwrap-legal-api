e6c3f2c3d9af120fdac3d2b716f86317788d1c2e
Thomas Schreiber <thomas.schreiber@gmx.at>
2025-05-02 13:18:00 +0200

    initial pojo civil case

---
+++ src/main/java/legal/shrinkwrap/api/adapter/ris/rest/dto/CaselawSummaryCivilCase.java
@@ -0,0 +1,22 @@
+package legal.shrinkwrap.api.adapter.ris.rest.dto;
+
+
+import java.util.List;
+
+public class CaselawSummaryCivilCase {
+    String art;
+    String ausgang;
+    String rechtsmittel;
+    String sachverhalt;
+    String begehren;
+    String gegenvorbringen;
+    List<String> zusammenfassung_3_absaetze;
+    List<String> zusammenfassung_3_saetze;
+    String zeitungstitel_boulevard;
+    String zeitungstitel_oeffentlich;
+    String zeitungstitel_rechtszeitschrift;
+    List<String> schlussfolgerungen;
+    List<String> wichtige_normen;
+    List<String> hauptrechtsgebiete;
+    List<String> unterrechtsgebiete;
+}
