d94bfa171cd9b2c0d58df26732eeb2f6e9e2ebf7
Thomas Schreiber <thomas.schreiber@gmx.at>
2025-06-13 20:52:39 +0200

    add minimal concurrency control for summaries

--- src/main/java/legal/shrinkwrap/api/controller/CaseLawController.java
+++ src/main/java/legal/shrinkwrap/api/controller/CaseLawController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import legal.shrinkwrap.api.dto.CaseLawRequestDto;
 import legal.shrinkwrap.api.dto.CaseLawResponseDto;
 import legal.shrinkwrap.api.service.DocumentService;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springdoc.core.annotations.ParameterObject;
@@ -14,12 +15,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.MessageFormat;
+import java.util.HashSet;
 
 @RestController
 @Validated
+@Slf4j
 public class CaseLawController {
 
     private static final Logger LOG = LoggerFactory.getLogger(CaseLawController.class);
+    private final HashSet<CaseLawRequestDto> locks = new HashSet<>();
 
     private final DocumentService documentService;
 
@@ -31,9 +35,30 @@ public class CaseLawController {
     public CaseLawResponseDto getShrinkwrapDocument(@Valid @ParameterObject CaseLawRequestDto requestDto) {
 
         LOG.info("Request {}", requestDto);
+        if (locks.contains(requestDto)) {
+            try {
+                //try for 40s, then do it anyway
+                for (int i=0;i<10*40;i++) {
+                    Thread.sleep(100);
+                    if (!locks.contains(requestDto)) {
+                        break;
+                    }
+                    log.info("trying to acquire lock " + i + " for " + requestDto);
+                }
 
-        CaseLawResponseDto document = documentService.getDocument(requestDto);
-        return document;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            locks.add(requestDto);
+        }
+        try {
+            CaseLawResponseDto document = documentService.getDocument(requestDto);
+            return document;
+        } finally {
+            locks.remove(requestDto);
+        }
+
     }
 
     @GetMapping("case-law/overview")
