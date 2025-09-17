2c0e5f0f951902540b0ca0c5cdcc06ba5e30905e
Thomas Schreiber <thomas.schreiber@gmx.at>
2025-09-17 08:09:32 +0200

    add redirect url for fresh extension installs

--- src/main/java/legal/shrinkwrap/api/controller/ShrinkwrapController.java
+++ src/main/java/legal/shrinkwrap/api/controller/ShrinkwrapController.java
@@ -5,13 +5,14 @@ import jakarta.annotation.PostConstruct;
 import legal.shrinkwrap.api.dto.GeneralDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class ShrinkwrapController {
-
     private static final Logger LOG = LoggerFactory.getLogger(ShrinkwrapController.class);
 
     @GetMapping("/hello")
@@ -19,6 +20,14 @@ public class ShrinkwrapController {
         return "Hello, I am a shrink wrapper";
     }
 
+
+    //on extension install, redirect to RIS
+    @GetMapping("/extension-install")
+    public RedirectView extensionInstall() {
+        RedirectView redirect = new RedirectView("https://www.ris.bka.gv.at/Judikatur/");
+        redirect.setStatusCode(HttpStatusCode.valueOf(303)); //always GET
+        return redirect;
+    }
 
     @GetMapping(value = "/general", produces = MediaType.APPLICATION_JSON_VALUE)
     public GeneralDto getGeneralInfo() {
