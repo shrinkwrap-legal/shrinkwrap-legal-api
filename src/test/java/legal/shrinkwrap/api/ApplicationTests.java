3a064bfbd6f0d53c45cf19ca7305be61c3c999d1
j3ernhard <bernhard.jungwirth@gmail.com>
2025-11-27 21:35:23 +0100

    tag for release build

--- src/test/java/legal/shrinkwrap/api/ApplicationTests.java
+++ src/test/java/legal/shrinkwrap/api/ApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @Tag("fast")
+@Tag("release")
 @SpringBootTest
 class ApplicationTests extends SpringTest {
 
